f8ad3df07cf49139d048837a6f18258254ab3466
undefined404offical <undefined404_@outlook.com>
2026-07-02 10:58:43 +0800

    删除了无用文件

--- programs/hello.c
+++
@@ -1,9 +0,0 @@
-extern void serial_puts(const char* s);
-
-int main(int argc, char** argv) {
-    serial_puts("================================\n");
-    serial_puts("  Hello from MWP program!\n");
-    serial_puts("  MWOS Program Format Demo\n");
-    serial_puts("================================\n");
-    return 0;
-}
